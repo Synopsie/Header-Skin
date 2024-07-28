89c958f099b2efe85abbc4194627448d36699c48
Dumont-Julien <dumontj357@gmail.com>
2024-07-28 11:44:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Arkania SkinSave Library
+
+La bibliothèque `SkinSave` est une solution PHP conçue pour faciliter la manipulation et la sauvegarde des skins de personnages pour Minecraft. Elle permet de valider, redimensionner et sauvegarder les données de skin sous forme d'images.
+
+## Fonctionnalités
+
+- Validation de la taille des skins.
+- Conversion des données de skin en images.
+- Redimensionnement des images de skin.
+- Sauvegarde des têtes de personnages à partir des données de skin.
+
+## Installation
+
+Vous mettez le fichier `SkinSave.php` dans votre projet et pensez à changer le namespace si besoin.
+
+## Utilisation
+
+### Valider la taille d'un skin
+
+Pour valider la taille d'un skin, utilisez la méthode `validateSize` :
+
+```php
+$isValid = \arkania\SkinSave::validateSize($size);
+```
+
+### Enregistrer un skin en config.
+
+Pour enregistrer un skin en config, utilisez la méthode `skinDataToImage` :
+
+```php
+$image = \arkania\SkinSave::skinDataToImage($skinData);
+```
+
+### Redimensionner une image de skin
+
+Pour redimensionner une image de skin, utilisez la méthode `resize_image` :
+
+```php
+imagepng(SkinSave::skinDataToImage($player->getSkin()->getSkinData()), Main::getInstance()->getDataFolder() . "skins/" . $player->getName() . ".png");
+```
+
+### Sauvegarder la tête d'un personnage
+
+Pour sauvegarder la tête d'un personnage à partir des données de skin, utilisez la méthode `savePlayerHead` :
+
+```php
+\arkania\SkinSave::savePlayerHead($playerName, $skinData, $path);
+```
+
+## Contribution
+
+Les contributions à la bibliothèque sont les bienvenues. Veuillez suivre les conventions de code standard PHP et soumettre vos pull requests pour examen.
+
+## Licence
+
+Cette bibliothèque est distribuée sous la licence MIT. Voir le fichier `LICENSE` pour plus d'informations.
---
+++ SkinSave.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+
+namespace arkania;
+
+class SkinSave {
+
+    public static array $acceptedSkinSize = [
+        64 * 32 * 4,
+        64 * 64 * 4,
+        128 * 128 * 4
+    ];
+
+    public static array $skin_widght_map = [
+        64 * 32 * 4 => 64,
+        64 * 64 * 4 => 64,
+        128 * 128 * 4 => 128
+    ];
+
+    public static array $skin_height_map = [
+        64 * 32 * 4 => 32,
+        64 * 64 * 4 => 64,
+        128 * 128 * 4 => 128
+    ];
+
+    public static function validateSize(int $size) : bool {
+        return in_array($size, self::$acceptedSkinSize);
+    }
+
+    public static function skinDataToImage($skinData, $customWidth = null, $customHeight = null) {
+        $size = strlen($skinData);
+        if (!self::validateSize($size)) {
+            return null;
+        }
+        $width = $customWidth ?? self::$skin_widght_map[$size];
+        $height = $customHeight ?? self::$skin_height_map[$size];
+        $image = imagecreatetruecolor($width, $height);
+        if ($image === false) {
+            return null;
+        }
+
+        imagefill($image, 0, 0, imagecolorallocatealpha($image, 0, 0, 0, 127));
+        $skinPos = 0;
+        for ($y = 0; $y < $height; $y++) {
+            for ($x = 0; $x < $width; $x++) {
+                $r = ord($skinData[$skinPos++]);
+                $g = ord($skinData[$skinPos++]);
+                $b = ord($skinData[$skinPos++]);
+                $a = 127 - intdiv(ord($skinData[$skinPos++]), 2);
+                $col = imagecolorallocatealpha($image, $r, $g, $b, $a);
+                imagesetpixel($image, $x, $y, $col);
+            }
+        }
+        imagesavealpha($image, true);
+        return $image;
+    }
+
+    public static function resize_image($file, $w, $h, $crop = true, $src_x = 0, $src_y = 0, $src_w = null, $src_h = null) {
+        [$width, $height] = getimagesize($file);
+        $src = imagecreatefrompng($file);
+        if (is_null($src_w) || is_null($src_h)) {
+            $src_w = $width;
+            $src_h = $height;
+        }
+        $dst = imagecreatetruecolor($w, $h);
+        imagecolortransparent($dst, imagecolorallocatealpha($dst, 0, 0, 0, 127));
+        imagealphablending($dst, false);
+        imagesavealpha($dst, true);
+        imagecopyresampled($dst, $src, 0, 0, $src_x, $src_y, $w, $h, $src_w, $src_h);
+        return $dst;
+    }
+
+    public static function savePlayerHead($playerName, $skinData, $path) : void {
+        $skinImage = self::skinDataToImage($skinData, 64, 64);
+        if ($skinImage === null) {
+            return;
+        }
+
+        $tempFilePath = tempnam(sys_get_temp_dir(), 'skin');
+        imagepng($skinImage, $tempFilePath);
+        imagedestroy($skinImage);
+
+        $headImage = self::resize_image($tempFilePath, 8, 8, false, 8, 8, 8, 8);
+
+        $headFilePath = $path . $playerName . ".png";
+        imagepng($headImage, $headFilePath);
+        imagedestroy($headImage);
+        unlink($tempFilePath);
+    }
+}
